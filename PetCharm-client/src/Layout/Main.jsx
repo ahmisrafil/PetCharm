02262cf7081a5611339ef0923e91d95fb3d256af
ahmisrafil <ahmisrafil@yahoo.com>
2024-01-23 00:12:13 +0600

    navbar added

--- PetCharm-client/src/Layout/Main.jsx
+++ PetCharm-client/src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer/Footer';
+import NavBar from './NavBar/NavBar';
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
