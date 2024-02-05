7c956b66427ff7b69533449723de2356370c62e0
ahmisrafil <ahmisrafil@yahoo.com>
2024-02-06 00:59:22 +0600

    featured category added

---
+++ PetCharm-client/src/components/Home/Featured_Catgory.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import img1 from '../../assets/category/parrot.webp'
+import img2 from '../../assets/category/cat_care.webp'
+import img3 from '../../assets/category/cat_food.webp'
+import img4 from '../../assets/category/cat_litter.webp'
+import img5 from '../../assets/category/cat-clotthing.webp'
+import img6 from '../../assets/category/dog_food.webp'
+const Featured_Catgory = () => {
+    return (
+        <div className='mx-20 my-10'>
+            <h3 className='text-3xl font-bold mb-10'>Featured Categories</h3>
+            <div className='flex flex-row gap-6 justify-between '>
+                <div>
+                    <img src={img1} className='rounded-full h-40' alt="" />
+                    <h3 className='font-bold text-center '>Birds Food</h3>
+                </div>
+                <div>
+                    <img src={img2} className='rounded-full h-40' alt="" />
+                    <h3 className='font-bold text-center '>Cats Health Suppliments</h3>
+                </div>
+                <div>
+                    <img src={img3} className='rounded-full h-40' alt="" />
+                    <h3 className='font-bold text-center '>Cats Dry Food</h3>
+                </div>
+                <div>
+                    <img src={img4} className='rounded-full h-40' alt="" />
+                    <h3 className='font-bold text-center '>Cats Treats</h3>
+                </div>
+                <div>
+                    <img src={img5} className='rounded-full h-40' alt="" />
+                    <h3 className='font-bold text-center '>Cats Fashion</h3>
+                </div>
+                <div>
+                    <img src={img6} className='rounded-full h-40' alt="" />
+                    <h3 className='font-bold text-center '>Dog Food</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured_Catgory;
--- PetCharm-client/src/pages/Home/Home/Home.jsx
+++ PetCharm-client/src/pages/Home/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Slider from '../../../components/Home/Slider';
+import Featured_Catgory from '../../../components/Home/Featured_Catgory';
 
 const Home = () => {
     return (
         <div className='min-h-screen'>
-            <div >
-            <Slider></Slider>
-            </div>
+         <Slider></Slider>
+         <Featured_Catgory></Featured_Catgory>
         </div>
     );
 };
