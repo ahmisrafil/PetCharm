489a0df88726d2667fef0e3a3fcd3c8ee273af25
ahmisrafil <ahmisrafil@yahoo.com>
2024-02-13 02:26:01 +0600

    offer page  added

--- PetCharm-client/src/pages/Home/Home/Home.jsx
+++ PetCharm-client/src/pages/Home/Home/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Slider from '../../../components/Home/Slider';
 import Featured_Catgory from '../../../components/Home/Featured_Catgory';
 import FeaturedItems from '../../../components/Home/FeaturedItems';
-
+import { Helmet} from 'react-helmet-async';
 const Home = () => {
     return (
         <div className='min-h-screen'>
+            <Helmet>
+                <title>PetCharm | Home</title>
+            </Helmet>
          <Slider></Slider>
          <Featured_Catgory></Featured_Catgory>
          <FeaturedItems></FeaturedItems>
--- PetCharm-client/src/pages/Offers/Offers.jsx
+++ PetCharm-client/src/pages/Offers/Offers.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../components/Shared/Cover';
+import coverImg from '../../assets/icons/cat-dog.png'
+import FeaturedItems from '../../components/Home/FeaturedItems';
+
+
 const Offers = () => {
     return (
-        <div>
+        <div className='mx-16'>
             <Helmet>
-                <title>Offers</title>
+                <title>PetCharm | Offers</title>
             </Helmet>
+            <Cover img={coverImg} title={"Our Offers"}></Cover>
+            <FeaturedItems></FeaturedItems>
         </div>
     );
 };
